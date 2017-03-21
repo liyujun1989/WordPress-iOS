ce1263e80b9da4ff60216b5e0cdbc64ed05d9e19
aerych <aerychj@gmail.com>
2017-03-21 17:43:32 -0400

    Adds SigninURLViewController

---
+++ WordPress/Classes/ViewRelated/NUX/SigninURLViewController.swift
@@ -0,0 +1,298 @@
+import UIKit
+import WordPressShared
+
+/// Provides a form and functionality to sign-in and add an existing self-hosted
+/// site to the app.
+///
+@objc class SigninURLViewController: NUXAbstractViewController, SigninKeyboardResponder, SigninWPComSyncHandler {
+    @IBOutlet weak var bottomContentConstraint: NSLayoutConstraint!
+    @IBOutlet weak var verticalCenterConstraint: NSLayoutConstraint!
+    @IBOutlet weak var siteURLField: WPWalkthroughTextField!
+    @IBOutlet weak var submitButton: NUXSubmitButton!
+    @IBOutlet weak var statusLabel: UILabel!
+
+
+    lazy var loginFacade: LoginFacade = {
+        let facade = LoginFacade()
+        facade.delegate = self
+        return facade
+    }()
+
+
+    lazy var xmlrpcFacade = WordPressXMLRPCAPIFacade()
+
+
+    override var sourceTag: SupportSourceTag {
+        get {
+            return .wpOrgLogin
+        }
+    }
+
+    /// A convenience method for obtaining an instance of the controller from a storyboard.
+    ///
+    /// - Parameter loginFields: A LoginFields instance containing any prefilled credentials.
+    ///
+    class func controller(_ loginFields: LoginFields) -> SigninURLViewController {
+        let storyboard = UIStoryboard(name: "Signin", bundle: Bundle.main)
+        let controller = storyboard.instantiateViewController(withIdentifier: "SigninURLViewController") as! SigninURLViewController
+        controller.loginFields = loginFields
+        return controller
+    }
+
+
+    // MARK: - Lifecycle Methods
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        localizeControls()
+        displayLoginMessage("")
+    }
+
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        // Update special case login fields.
+        loginFields.userIsDotCom = false
+
+        configureTextFields()
+        configureSubmitButton(animating: false)
+        configureViewForEditingIfNeeded()
+    }
+
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        registerForKeyboardEvents(keyboardWillShowAction: #selector(SigninEmailViewController.handleKeyboardWillShow(_:)),
+                                  keyboardWillHideAction: #selector(SigninEmailViewController.handleKeyboardWillHide(_:)))
+
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        unregisterForKeyboardEvents()
+    }
+
+
+    // MARK: Setup and Configuration
+
+
+    /// Assigns localized strings to various UIControl defined in the storyboard.
+    ///
+    func localizeControls() {
+        siteURLField.placeholder = NSLocalizedString("Site Address (URL)", comment: "Site Address placeholder")
+
+        let submitButtonTitle = NSLocalizedString("Add Site", comment: "Title of a button. The text should be uppercase.").localizedUppercase
+        submitButton.setTitle(submitButtonTitle, for: UIControlState())
+        submitButton.setTitle(submitButtonTitle, for: .highlighted)
+    }
+
+
+    /// Configures the content of the text fields based on what is saved in `loginFields`.
+    ///
+    func configureTextFields() {
+        siteURLField.text = loginFields.siteUrl
+    }
+
+
+    /// Displays the specified text in the status label.
+    ///
+    /// - Parameters:
+    ///     - message: The text to display in the label.
+    ///
+    func configureStatusLabel(_ message: String) {
+        statusLabel.text = message
+    }
+
+
+    /// Configures the appearance and state of the submit button.
+    ///
+    func configureSubmitButton(animating: Bool) {
+        submitButton.showActivityIndicator(animating)
+
+        submitButton.isEnabled = (
+            !animating && !loginFields.siteUrl.isEmpty
+        )
+    }
+
+
+    /// Sets the view's state to loading or not loading.
+    ///
+    /// - Parameter loading: True if the form should be configured to a "loading" state.
+    ///
+    func configureViewLoading(_ loading: Bool) {
+        siteURLField.isEnabled = !loading
+
+        configureSubmitButton(animating: loading)
+
+        navigationItem.hidesBackButton = loading
+    }
+
+
+    /// Configure the view for an editing state. Should only be called from viewWillAppear
+    /// as this method skips animating any change in height.
+    ///
+    func configureViewForEditingIfNeeded() {
+        // Check the helper to determine whether an editiing state should be assumed.
+        adjustViewForKeyboard(SigninEditingState.signinEditingStateActive)
+        if SigninEditingState.signinEditingStateActive {
+            siteURLField.becomeFirstResponder()
+        }
+    }
+
+
+    // MARK: - Instance Methods
+
+
+    ///
+    ///
+    func updateSafariCredentialsIfNeeded() {
+        // Noop.  Required by the SigninWPComSyncHandler protocol but the self-hosted
+        // controller's implementation does not use safari saved credentials.
+    }
+
+
+    /// Validates what is entered in the various form fields and, if valid,
+    /// proceeds with the submit action.
+    ///
+    func validateForm() {
+        view.endEditing(true)
+
+        if !SigninHelpers.validateSiteForSignin(loginFields) {
+            WPError.showAlert(withTitle: NSLocalizedString("Error", comment: "Title of an error message"),
+                              message: NSLocalizedString("The site's URL appears to be mistyped", comment: "A short prompt alerting to a misformatted URL"),
+                              withSupportButton: false)
+
+            return
+        }
+
+        configureViewLoading(true)
+
+        xmlrpcFacade.guessXMLRPCURL(forSite: loginFields.siteUrl, success: {[ weak self] (url) in
+            // Found the URL.
+            self?.didValidateForm(url: url)
+            self?.configureViewLoading(false)
+
+        }, failure: { [weak self] (error) in
+            self?.configureViewLoading(false)
+            self?.handleXMLRPCError(error: error as NSError)
+        })
+    }
+
+
+    func didValidateForm(url: URL) {
+        loginFields.siteUrl = url.absoluteString
+
+        let controller: UIViewController
+        if loginFields.siteUrl.contains("wordpress.com/xmlrpc.php") {
+            // Show WordPress.com flow
+            controller = SigninWPComViewController.controller(loginFields)
+        } else {
+            // Show selfhosted flow
+            controller = SigninSelfHostedViewController.controller(loginFields)
+        }
+        navigationController?.pushViewController(controller, animated: true)
+    }
+
+
+    func handleXMLRPCError(error: NSError) {
+        // TODO: Different error can occur.
+
+
+        // Network unavailable.
+
+
+        // No site found
+
+
+        // Site found not WordPress site
+
+
+        // Other HTTP 500 or 400 error.
+
+
+        // Request timed out.
+
+
+    }
+
+    // MARK: - Actions
+
+
+    @IBAction func handleTextFieldDidChange(_ sender: UITextField) {
+        loginFields.siteUrl = SigninHelpers.baseSiteURL(string: siteURLField.nonNilTrimmedText())
+
+        configureSubmitButton(animating: false)
+    }
+
+
+    @IBAction func handleSubmitButtonTapped(_ sender: UIButton) {
+        validateForm()
+    }
+
+
+    // MARK: - Keyboard Notifications
+
+
+    func handleKeyboardWillShow(_ notification: Foundation.Notification) {
+        keyboardWillShow(notification)
+    }
+
+
+    func handleKeyboardWillHide(_ notification: Foundation.Notification) {
+        keyboardWillHide(notification)
+    }
+}
+
+
+extension SigninURLViewController: LoginFacadeDelegate {
+
+    func finishedLogin(withUsername username: String!, authToken: String!, requiredMultifactorCode: Bool) {
+        syncWPCom(username, authToken: authToken, requiredMultifactor: requiredMultifactorCode)
+    }
+
+
+    func finishedLogin(withUsername username: String!, password: String!, xmlrpc: String!, options: [AnyHashable: Any]!) {
+        displayLoginMessage("")
+
+        BlogSyncFacade().syncBlog(withUsername: username, password: password, xmlrpc: xmlrpc, options: options) { [weak self] in
+            self?.configureViewLoading(false)
+
+            let context = ContextManager.sharedInstance().mainContext
+            let service = BlogService(managedObjectContext: context)
+            if let blog = service.findBlog(withXmlrpc: xmlrpc, andUsername: username) {
+                service.flagBlog(asLastUsed: blog)
+            }
+
+            NotificationCenter.default.post(name: Foundation.Notification.Name(rawValue: SigninHelpers.WPSigninDidFinishNotification), object: nil)
+
+            self?.dismiss()
+        }
+    }
+
+
+    func displayLoginMessage(_ message: String!) {
+        configureStatusLabel(message)
+    }
+
+
+    func displayRemoteError(_ error: Error!) {
+        displayLoginMessage("")
+        configureViewLoading(false)
+        displayError(error as NSError, sourceTag: sourceTag)
+    }
+
+}
+
+
+extension SigninURLViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if submitButton.isEnabled {
+            validateForm()
+        }
+        return true
+    }
+}
