5913219da1fdca6a3e6679331f2fadb6a6cf2ee7
aerych <aerychj@gmail.com>
2017-03-21 17:42:56 -0400

    Adds nullability

--- WordPress/Classes/Services/WordPressXMLRPCAPIFacade.h
+++ WordPress/Classes/Services/WordPressXMLRPCAPIFacade.h
@@ -1,6 +1,7 @@
 #import <Foundation/Foundation.h>
 
 @protocol WordPressXMLRPCAPIFacade
+NS_ASSUME_NONNULL_BEGIN
 
 - (void)guessXMLRPCURLForSite:(NSString *)url
                       success:(void (^)(NSURL *xmlrpcURL))success
@@ -12,6 +13,7 @@
                           success:(void (^)(id options))success
                           failure:(void (^)(NSError *error))failure;
 
+NS_ASSUME_NONNULL_END
 @end
 
 @interface WordPressXMLRPCAPIFacade : NSObject<WordPressXMLRPCAPIFacade>
